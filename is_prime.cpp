d71ace230d9ddf9ae5a0718cfe5d3bfcfdcde93b
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-05-19 20:26:47 +0530

    Create is_prime.cpp

---
+++ is_prime.cpp
@@ -0,0 +1,24 @@
+#include<iostream> 
+using namespace std; 
+int main(){
+  int n; 
+  cin>> n; 
+  int fal; 
+
+  for(int i=2;i<n;i++){
+    if(n%i==0){
+      cout<<"not a prime number"; 
+      break; 
+      }
+else{cout<<"is a prime "; 
+fal=1; 
+if(fal==1){
+  break; 
+}
+    } 
+
+
+
+}
+  
+    } 
