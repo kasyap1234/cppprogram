174ef18853389537b429fd1796367ea61440eff1
kasyap dharanikota <76957823+kasyap1234@users.noreply.github.com>
2021-10-27 17:42:29 +0530

    Create tl.cpp

---
+++ tl.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std; 
+#include<string>
+
+#include<bits/stdc++.h>
+
+
+
+int main(){
+stack<string>st; 
+int n; 
+    string s; 
+for(int i=0;i<n;i++){
+    getline(cin,s); 
+st.push(s); 
+}
+   int check=st.size(); 
+    
+
+    
+    
+    
+int m; 
+cin>>m; 
+for(int i=0;i<m; i++){
+st.pop(); 
+}
+
+    
+    
+}
+
+
+
+
